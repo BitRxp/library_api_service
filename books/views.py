34728b95bb0e3b1f52c0fb277ae9580822995cce
Alex <oleksandrtaranenko239@gmail.com>
2024-12-03 21:02:58 +0200

    add permission_classes and delete serializer_class

--- books/views.py
+++ books/views.py
@@ -1,15 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from books.models import Book
 from books.serializers import (
     BookSerializer,
     BookListSerializer
 )
+from books.premissions import IsAdminOrReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_serializer_class(self):
         if self.action == "list":
