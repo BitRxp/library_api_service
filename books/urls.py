4f73d2c2f2e29e1cc8fbace9bed383d549f2aba9
Alex <oleksandrtaranenko239@gmail.com>
2024-12-03 20:28:49 +0200

    deleted prefix "books" from url

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ from books.views import (
 )
 
 router = routers.DefaultRouter()
-router.register("books", BookViewSet)
+router.register("", BookViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
