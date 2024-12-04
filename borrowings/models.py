06e7b671a1f71009841da1c5eb2834a8b0ee589f
Alex <oleksandrtaranenko239@gmail.com>
2024-12-04 12:30:36 +0200

    add validate_dates and clean and save and return_book

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
+from rest_framework.exceptions import ValidationError
 
 from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
-    expected_return = models.DateField()
-    actual_return = models.DateField(null=True, blank=True)
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True, default=None)
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
@@ -19,9 +22,48 @@ class Borrowing(models.Model):
         related_name="borrowings",
     )
 
-    def __str__(self):
-        return (
-            f"{self.user}"
-            f" borrowed {self.book}"
-            f" on {self.borrow_date}"
+    @staticmethod
+    def validate_dates(
+            borrow_date, expected_return_date, actual_return_date, error_to_raise
+    ):
+        borrow_date = borrow_date or timezone.now().date()
+        if borrow_date >= expected_return_date:
+            raise error_to_raise(
+                f"Borrow date should be earlier than expected return date. "
+                f"Borrow date is {borrow_date}, "
+                f"and expected return date is {expected_return_date}."
+            )
+
+        if actual_return_date and borrow_date >= actual_return_date:
+            raise error_to_raise(
+                f"Borrow date should be earlier than actual return date. "
+                f"Borrow date is {borrow_date}, "
+                f"and actual return date is {actual_return_date}."
+            )
+
+    def clean(self):
+        Borrowing.validate_dates(
+            self.borrow_date,
+            self.expected_return_date,
+            self.actual_return_date,
+            ValidationError,
         )
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            if self.book.inventory < 1:
+                raise ValidationError("The book is not available.")
+            self.full_clean()
+            self.book.inventory -= 1
+            self.book.save()
+        super().save(*args, **kwargs)
+
+    def return_book(self):
+        if not self.actual_return_date:
+            self.actual_return_date = timezone.now().date()
+            self.book.inventory += 1
+            self.book.save()
+            self.save()
+
+    def __str__(self):
+        return f"{self.book} {self.borrow_date} — {self.expected_return_date}"
